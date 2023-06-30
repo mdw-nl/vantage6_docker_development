bebc5d3bb39b18a70eadcf24a4b67f5b91f984ad
Luis Sago <lws.sago@gmail.com>
2023-06-30 11:17:33 +0200

    Fix: task execution bug due to algorithm missing access to data volume
    These changes are to keep up with vantage6_docker/65c934b4ce

--- dev-v6-server/pre_run.sh
+++ dev-v6-server/pre_run.sh
@@ -7,7 +7,7 @@ source /start-up.d/common/lib.sh
 dev_setup() {
     echo "Setting up dev environment"
     # TODO: '-e dev' is dependent on mdw-nl/v6server_docker hardcoding dev too, we will change that
-    vserver-local import /start-up-hooks.d/entities.yaml --config /etc/xdg/vantage6/config.yaml -e dev
+    vserver-local import /start-up-hooks.d/entities.yaml --config /mnt/config/config.yaml -e dev
 }
 
 main() {
